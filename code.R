a5f2b31a22a57bc2c3b53fcdc26de0a469b6381d
alyssapradhan <150135829+alyssapradhan@users.noreply.github.com>
2024-04-15 14:05:35 -0300

    Filter to < 30

--- code.R
+++ code.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(here)
 
 tvhours_table <- gss_cat %>% group_by(marital) %>% 
-  filter(age >= 30) %>% 
+  filter(age < 30) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
 write.csv(tvhours_table, here("TV_hours_marital.csv"))
